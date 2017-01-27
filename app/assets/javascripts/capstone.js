3838c39d978c562cbf3f229bd2c8249783a5bd07
Alberto Penas Amor <alberto__pa@hotmail.com>
2017-01-27 18:12:42 +0100

    added app module added app constants added app router added manifest dependencies

--- app/assets/javascripts/capstone.js
+++ app/assets/javascripts/capstone.js
@@ -4,3 +4,6 @@
 //= require angular
 //= require angular-ui-router
 //= require angular-resource
+//= require capstone/app.module
+//= require capstone/app.router
+//= require capstone/app.constant
---
+++ app/assets/javascripts/capstone/app.module.js
@@ -0,0 +1,4 @@
+(function() {
+  'use-strict';
+  angular.module("capstone",["ui.router"]);
+})();
