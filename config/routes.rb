fc6d9d0f3a0d1e520b298783fdd9a6e52f23f9a7
Alberto Penas Amor <alberto__pa@hotmail.com>
2017-01-28 02:17:10 +0100

    added crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   scope :api, defaults: { format: :json } do
-    resources :cities, except: [:new, :edit, :update, :destroy, :create]
-    resources :states, except: [:new, :edit, :update, :destroy, :create]
+    resources :cities, except: [:new, :edit]
+    resources :states, except: [:new, :edit]
   end
   get '/ui' => 'ui#index'
   get '/ui#' => 'ui#index'
